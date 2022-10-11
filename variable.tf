7cdce36a4697d27a0795bb1dcf11fbdad4a20163
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-11 21:35:39 +0530

    Required Variables info

--- variable.tf
+++ variable.tf
@@ -1,1 +1,13 @@
-
+variable "az_vnet_name" {
+   description = "(Required) Name of the Azure Virtual Network"
+   default     = "cloud-vnet"
+}
+variable "address_space"{
+   description = "Address Space Used by the virtual network"
+   type        = string
+   default     = "10.0.3.0/24"
+}
+variable "resource_group_name" {
+    description = "Name of the resource group"
+    type        = string
+}
