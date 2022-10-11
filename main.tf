de7754b4d81cb1cc2c03f4e5d1534dd718522cc2
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-11 21:40:25 +0530

    formatting code

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+data "azurerm_resource_group" "rg"{
+  name = var.name
+}
 
+resource "azurerm_virtual_network" "myvnet"{
+  name                = var.az_vnet_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  address_space       = var.address_space
+}
+
+  
