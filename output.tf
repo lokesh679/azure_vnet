88826b1bec14ef1535f2059b1535f1a82bcac56c
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-11 22:00:30 +0530

    updated output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,16 @@
-
+output "az_vnet_name" { 
+  description = "Name of the Created vnet"
+  value       = azurerm_virtual_network.az_vnet_name.name
+}
+output "az_vnet_id"{
+  description = "Virtual Network ID"
+  value       = azurerm_virtual_network.myvnet.id
+}
+output "az_vnet_location"{
+  desrciption = "Location of the newly created vnet"
+  value       = azurerm_virtual_network.myvnett.location
+}
+output "az_vnet_address"{
+  desrciption = "Address Space of the newly created vnet"
+  value       = azurerm_virtual_network.myvnet.address_space
+}
